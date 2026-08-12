b5c1101e11883b602db3774d2d7c7f22ce5ef668
陈希远 <chenxiyuan@macbook.lan>
2026-08-12 23:48:18 +0800

    fix: 可视化分析 embed 模式显式启用搜索框和时间筛选

--- web/src/pages/Viz.tsx
+++ web/src/pages/Viz.tsx
@@ -3,7 +3,7 @@ const NDR_HOME_DASHBOARD = "a8411b30-6d03-11ea-b301-3d6c35840645";
 const KIBANA_URL =
   "/kibana/app/dashboards#/view/" +
   NDR_HOME_DASHBOARD +
-  "?embed=true&hide-filter-bar=false&viewMode=view&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-24h,to:now))";
+  "?embed=true&show-query-input=true&show-time-filter=true&hide-filter-bar=false&viewMode=view&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-24h,to:now))";
 
 export default function Viz() {
   return (
